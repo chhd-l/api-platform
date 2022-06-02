bda93af7141037f4b6da4bd954af9a3a191f9adc
tutu <1515112287@qq.com>
2022-06-02 18:00:30 +0800

    search页面

--- src/components/common/Layout/Header/SearchHeader/index.tsx
+++ src/components/common/Layout/Header/SearchHeader/index.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { HeaderBox } from '../components/HeaderBox'
 import LOGO_white from '../../../../../assets/image/LOGO_white.png'
+import user from '../../../../../assets/image/user.png'
 import { SearchTab } from './components/SearchTab'
 
 const Img = styled.img`
@@ -12,8 +13,9 @@ export const SearchHeader = () => {
   return (
     <HeaderBox>
       <div className="w-am1200 m-auto h-am70 flex items-center ">
-        <Img src={LOGO_white} />
+        <Img src={LOGO_white} className="mr-14" />
         <SearchTab />
+        <img src={user} alt="" className="" />
       </div>
     </HeaderBox>
   )
